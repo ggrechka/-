cf6c0d5a19b3472e995466a94c0e99ceabf2e992
Grechka <kurchenko-e@mail.ru>
2020-09-21 11:01:52 +0300

    V2
    Доработка текущей реализации

--- Stack V1/Stack/Form1.Designer.cs
+++ Stack V1/Stack/Form1.Designer.cs
@@ -38,6 +38,18 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textMin = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textMax = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textSum = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textMulti = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBottom = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textIn = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // pushButton
@@ -47,7 +59,7 @@
             this.pushButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.pushButton.Font = new System.Drawing.Font("Druk Text Wide Bold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.pushButton.ForeColor = System.Drawing.Color.Transparent;
-            this.pushButton.Location = new System.Drawing.Point(34, 146);
+            this.pushButton.Location = new System.Drawing.Point(23, 146);
             this.pushButton.Name = "pushButton";
             this.pushButton.Size = new System.Drawing.Size(222, 30);
             this.pushButton.TabIndex = 0;
@@ -62,9 +74,9 @@
             this.popButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.popButton.Font = new System.Drawing.Font("Druk Text Wide Bold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.popButton.ForeColor = System.Drawing.Color.Transparent;
-            this.popButton.Location = new System.Drawing.Point(339, 146);
+            this.popButton.Location = new System.Drawing.Point(23, 182);
             this.popButton.Name = "popButton";
-            this.popButton.Size = new System.Drawing.Size(196, 30);
+            this.popButton.Size = new System.Drawing.Size(224, 31);
             this.popButton.TabIndex = 1;
             this.popButton.Text = "Удалить элемент";
             this.popButton.UseVisualStyleBackColor = false;
@@ -74,16 +86,16 @@
             // 
             this.elementsTextBox.BackColor = System.Drawing.SystemColors.HighlightText;
             this.elementsTextBox.Font = new System.Drawing.Font("Druk Text Wide Bold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.elementsTextBox.Location = new System.Drawing.Point(673, 122);
+            this.elementsTextBox.Location = new System.Drawing.Point(669, 122);
             this.elementsTextBox.Name = "elementsTextBox";
-            this.elementsTextBox.Size = new System.Drawing.Size(416, 343);
+            this.elementsTextBox.Size = new System.Drawing.Size(416, 184);
             this.elementsTextBox.TabIndex = 2;
             this.elementsTextBox.Text = "";
             // 
             // elementTextBox
             // 
             this.elementTextBox.Font = new System.Drawing.Font("Druk Text Wide Bold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.elementTextBox.Location = new System.Drawing.Point(34, 103);
+            this.elementTextBox.Location = new System.Drawing.Point(23, 103);
             this.elementTextBox.Name = "elementTextBox";
             this.elementTextBox.Size = new System.Drawing.Size(222, 28);
             this.elementTextBox.TabIndex = 3;
@@ -94,7 +106,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Druk Text Wide Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(34, 77);
+            this.label1.Location = new System.Drawing.Point(23, 77);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(224, 23);
             this.label1.TabIndex = 4;
@@ -103,9 +115,9 @@
             // textTop
             // 
             this.textTop.Font = new System.Drawing.Font("Druk Text Wide Bold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textTop.Location = new System.Drawing.Point(339, 103);
+            this.textTop.Location = new System.Drawing.Point(340, 103);
             this.textTop.Name = "textTop";
-            this.textTop.Size = new System.Drawing.Size(196, 28);
+            this.textTop.Size = new System.Drawing.Size(218, 28);
             this.textTop.TabIndex = 5;
             // 
             // label2
@@ -114,7 +126,7 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Druk Text Wide Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(335, 77);
+            this.label2.Location = new System.Drawing.Point(340, 77);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(207, 23);
             this.label2.TabIndex = 6;
@@ -144,6 +156,128 @@
             this.label4.TabIndex = 8;
             this.label4.Text = "Содержимое стека:";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Druk Text Wide Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(19, 356);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(185, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Минимальный";
+            // 
+            // textMin
+            // 
+            this.textMin.Font = new System.Drawing.Font("Druk Text Wide Bold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textMin.Location = new System.Drawing.Point(19, 382);
+            this.textMin.Name = "textMin";
+            this.textMin.Size = new System.Drawing.Size(222, 28);
+            this.textMin.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Druk Text Wide Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(336, 356);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(196, 23);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Максимальный";
+            // 
+            // textMax
+            // 
+            this.textMax.Font = new System.Drawing.Font("Druk Text Wide Bold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textMax.Location = new System.Drawing.Point(336, 382);
+            this.textMax.Name = "textMax";
+            this.textMax.Size = new System.Drawing.Size(218, 28);
+            this.textMax.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Druk Text Wide Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(19, 487);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 23);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Сумма";
+            // 
+            // textSum
+            // 
+            this.textSum.Font = new System.Drawing.Font("Druk Text Wide Bold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textSum.Location = new System.Drawing.Point(19, 513);
+            this.textSum.Name = "textSum";
+            this.textSum.Size = new System.Drawing.Size(222, 28);
+            this.textSum.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Druk Text Wide Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(336, 487);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(187, 23);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Произведение";
+            // 
+            // textMulti
+            // 
+            this.textMulti.Font = new System.Drawing.Font("Druk Text Wide Bold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textMulti.Location = new System.Drawing.Point(336, 513);
+            this.textMulti.Name = "textMulti";
+            this.textMulti.Size = new System.Drawing.Size(218, 28);
+            this.textMulti.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Druk Text Wide Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(340, 136);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(130, 23);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Дно стека";
+            // 
+            // textBottom
+            // 
+            this.textBottom.Font = new System.Drawing.Font("Druk Text Wide Bold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBottom.Location = new System.Drawing.Point(340, 162);
+            this.textBottom.Name = "textBottom";
+            this.textBottom.Size = new System.Drawing.Size(218, 28);
+            this.textBottom.TabIndex = 17;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Druk Text Wide Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(669, 316);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(369, 23);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Содержимое инверсии стека:";
+            // 
+            // textIn
+            // 
+            this.textIn.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textIn.Font = new System.Drawing.Font("Druk Text Wide Bold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textIn.Location = new System.Drawing.Point(669, 357);
+            this.textIn.Name = "textIn";
+            this.textIn.Size = new System.Drawing.Size(416, 184);
+            this.textIn.TabIndex = 19;
+            this.textIn.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -151,7 +285,19 @@
             this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1101, 517);
+            this.ClientSize = new System.Drawing.Size(1101, 596);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textIn);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBottom);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textMulti);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textSum);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textMax);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textMin);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -183,6 +329,18 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textMin;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textMax;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textSum;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textMulti;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBottom;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RichTextBox textIn;
     }
 }
 
--- Stack V1/Stack/Form1.cs
+++ Stack V1/Stack/Form1.cs
@@ -1,118 +1,221 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Windows.Forms;
 
 namespace Stack
 {
     public partial class Form1 : Form
     {
-        Stack stack;
-        
+        Stack stack = new Stack();
+        Stack2 stack2 = new Stack2();
+
         public Form1()
         {
             InitializeComponent();
             stack = new Stack();
-    }
+            stack2 = new Stack2();
+        }
 
         private void pushButton_Click(object sender, EventArgs e)
         {
+            //добавление элемента в стек
             stack.Push(Convert.ToInt32(elementTextBox.Text));
+            //печать элементов
             elementsTextBox.Text = stack.Print();
-            //int counter = 0;
-            //while (true)
-            //{
-            //    stack.Push(int.MaxValue);
-            //    counter++;
-            //    elementsTextBox.AppendText(Convert.ToString(counter));
-            //}
+
+            //поиск минимумов-максимумов
             textTop.Text = stack.PrintTop();
+            if (stack.SearchMin() == 0)
+                textMin.Text = "Не найдено";
+            else
+                textMin.Text = Convert.ToString(stack.SearchMin());
+
+            if (stack.SearchMax() == 0)
+                textMax.Text = "Не найдено";
+            else
+                textMax.Text = Convert.ToString(stack.SearchMax());
+
+            //поиск суммы и произведения
+            textSum.Text = Convert.ToString(stack.SearchSum());
+            textMulti.Text = Convert.ToString(stack.SearchMulti());
+
+            //печать инверсии стека
+            textBottom.Text = Convert.ToString(stack.items[stack2.bot]);
+            stack.Inversion(stack2);
+            textIn.Text = stack2.Print();
         }
 
         private void popButton_Click(object sender, EventArgs e)
         {
+            //удаление элемента и печать стека
             stack.Pop();
             elementsTextBox.Text = stack.Print();
             textTop.Text = stack.PrintTop();
-        }
-    }
 
-    public class Stack
-    {
-        private int[] items;
-        private int counter;
-        const int n = 10;
+            //поиск минимумов-максимумов
+            textTop.Text = stack.PrintTop();
+            if (stack.SearchMin() == 0)
+                textMin.Text = "Не найдено";
+            else
+                textMin.Text = Convert.ToString(stack.SearchMin());
 
-        public Stack()
-        {
-            items = new int[n];
-            counter = 0;
-        }
-        //public Stack(int stackCount)
-        //{
-        //    items = new int[stackCount];
-        //    counter = 0;
-        //}
+            if (stack.SearchMax() == 0)
+                textMax.Text = "Не найдено";
+            else
+                textMax.Text = Convert.ToString(stack.SearchMax());
 
-        //public Stack(int[] stackArray)
-        //{
-        //    items = stackArray;
-        //    counter = stackArray.Length;
-        //}
-
-        public bool IsEmpty()
-        {
-            return counter == 0;
+            //печать суммы и произведения 
+            textSum.Text = Convert.ToString(stack.SearchSum());
+            textMulti.Text = Convert.ToString(stack.SearchMulti());
+            
+            //инверсия и печать
+            textBottom.Text = Convert.ToString(stack2.bot);
+            stack.Inversion(stack2);
+            textIn.Text = stack2.Print();
         }
 
-        public void Push(int item)
+
+
+
+        public class Stack
         {
-            if (counter == items.Length)
+            public int[] items;
+            public int counter;
+            public const int n = 10;
+
+            public Stack()
             {
-                Resize(items.Length + 10);
-                MessageBox.Show("Стек переполнен, его размер увеличен");
+                items = new int[n];
+                counter = 0;
             }
 
-            items[counter++] = item;
-        }
-
-        private void Resize(int size)
-        {
-            int[] tempItems = new int[size];
-            for (int i = 0; i < counter; ++i)
-                tempItems[i] = items[i];
-            items = tempItems;
-        }
-
-        public int Pop()
-        {
-            if (IsEmpty())
+            //проверка на пустоту
+            public bool IsEmpty()
             {
-                MessageBox.Show("Стек пуст");
+                return counter == 0;
+            }
+            
+            //добавление элемента
+            public void Push(int item)
+            {
+                if (counter == items.Length)
+                {
+                    Resize(items.Length + 10);
+                    MessageBox.Show("Стек переполнен, его размер увеличен");
+                }
+
+                items[counter++] = item;
+            }
+            //изменение размера стека
+            public void Resize(int size)
+            {
+                int[] tempItems = new int[size];
+                for (int i = 0; i < counter; ++i)
+                    tempItems[i] = items[i];
+                items = tempItems;
+            }
+            //удаление элемента
+            public int Pop()
+            {
+                if (IsEmpty())
+                {
+                    MessageBox.Show("Стек пуст");
+
+                    return 0;
+                }
+                int item = items[--counter];
+                if (0 < counter && counter < items.Length - 10)
+                    Resize(items.Length - 10);
+                return item;
+
+            }
+            //возвращение строкового вида стека
+            public string Print()
+            {
+                if (counter == 0)
+                    return "";
+                int i;
+                string s = "";
+                for (i = 0; i < counter - 1; ++i)
+                    s += Convert.ToString(items[i]) + ", ";
+                s += Convert.ToString(items[i]);
+                return s;
+            }
+            // возврат верхушки
+            public string PrintTop()
+            {
+                int number = counter - 1;
+                return Convert.ToString(items[number]);
+            }
+            //поиск минимума
+            public int SearchMin()
+            {
+                if (counter == 0)
+                    return 0;
+                int i, min = items[0];
+                for (i = 1; i < counter; ++i)
+                    if (items[i] < min)
+                        min = items[i];
+                return min;
+            }
+            //поиск максимума
+            public int SearchMax()
+            {
+                if (counter == 0)
+                    return 0;
+                int i, max = items[0];
+                for (i = 1; i < counter; ++i)
+                    if (items[i] > max)
+                        max = items[i];
+                return max;
+            }
+            //подсчёт суммы
+            public int SearchSum()
+            {
+                int i, sum = 0;
+                for (i = 0; i < counter; ++i)
+                    sum += items[i];
+                return sum;
+            }
+            //подсчёт произведения
+            public int SearchMulti()
+            {
+                int i, p = 1;
+                for (i = 0; i < counter; ++i)
+                    p *= items[i];
+                return p;
+            }
+            //инверсия стека
+            public void Inversion(Stack2 stack2)
+            {
+                int i, j;
+                if (this.counter>10) 
+                    stack2.Resize(counter);
+                for (i = this.counter-1, j = 0; i > -1; i--, j++)
+
+                    stack2.items[j] = this.items[i];
+                stack2.counter = this.counter;
                 
+                
+            }
+        }
+
+        public class Stack2 : Stack
+        {
+            public int bot;
+
+            public Stack2()
+            {
+                items = new int[n];
+                counter = 0;
+                bot = 0;
+            }
+            public int Bottom()
+            {
+                if (!IsEmpty())
+                    return items[bot];
                 return 0;
             }
-            int item = items[--counter];
-            if (0 < counter && counter < items.Length - 10)
-                Resize(items.Length - 10);
-            return item;
-            
-        }
-
-        public string Print()
-        {
-            if (counter == 0)
-                return "";
-            int i;
-            string s = "";
-            for (i = 0; i < counter - 1; ++i)
-                s += Convert.ToString(items[i]) + ", ";
-            s += Convert.ToString(items[i]);
-            return s;
-        }
-
-        public string PrintTop()
-        {
-            int number = counter - 1;
-            return Convert.ToString(items[number]);
         }
     }
 }
